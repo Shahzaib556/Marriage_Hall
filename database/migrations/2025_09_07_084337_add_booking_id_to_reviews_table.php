4c0327d53ef480a6d0ea1441d56d727bf7ff9e09
Usman-Ali78 <usmanm.ali78@gmail.com>
2025-09-07 23:25:41 +0500

    added booking id

---
+++ database/migrations/2025_09_07_084337_add_booking_id_to_reviews_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  public function up(): void
+    {
+    Schema::table('reviews', function (Blueprint $table) {
+        $table->unsignedBigInteger('booking_id')->after('hall_id')->nullable();
+
+        $table->foreign('booking_id')
+              ->references('id')
+              ->on('bookings')
+              ->onDelete('cascade');
+    });
+    }
+
+public function down(): void
+    {
+    Schema::table('reviews', function (Blueprint $table) {
+        $table->dropForeign(['booking_id']);
+        $table->dropColumn('booking_id');
+    });
+    }
+};
